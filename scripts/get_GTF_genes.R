8ae6423b1e6609f5b0719041346db2f3e3c25912
Franke <vedran.franke@gmail.com>
2018-06-20 18:59:40 +0200

    scripts/get_GTF_genes.R: added additional columns for convert to refFlat

--- scripts/get_GTF_genes.R
+++ scripts/get_GTF_genes.R
@@ -11,22 +11,41 @@ argv = Parse_Arguments('get_GTF_genes')
 # -------------------------------------------------------------------------- #
 library(rtracklayer)
 library(GenomicRanges)
-get_GTF_genes <- function(gtfFile = NULL,
-                          output = NULL) {
+
+get_GTF_genes = function(
+    gtfFile = NULL,
+    output  = NULL
+
+) {
     if (is.null(gtfFile))
         stop("gtfFile not specified")
     if (is.null(output))
         stop("output file not specified")
-    gtf <- import.gff2(gtfFile)
-    g <- subset(gtf, type == "exon")
-    gl <- split(g, g$gene_id)
-    gl <- unlist(range(gl))
-    
-    export(gl, output, "gtf")
+
+    gtf   = import.gff2(gtfFile)
+    g     = subset(gtf, type == "exon")
+    annot = values(gtf)[,c('gene_id','gene_name')]
+    annot = unique(annot)
+    annot$exon_id         = annot$gene_id
+    annot$transcript_id   = annot$gene_id
+    annot$transcript_name = annot$gene_id
+    annot$type = 'exon'
+    annot$exon_number = 1
+
+    gl  = split(g, g$gene_id)
+    gl  = unlist(range(gl))
+    gl$gene_id = names(gl)
+    values(gl) = merge(values(gl), annot, by='gene_id')
+
+
+    if(!all(names(gl) == gl$gene_id))
+        stop('gene_id is not properly ordered')
+
+    export.gff2(gl, output)
 }
 
 # -------------------------------------------------------------------------- #
 get_GTF_genes(
     gtfFile = argv$input[['infile']],
-    output = argv$output[['outfile']]
+    output  = argv$output[['outfile']]
 )
