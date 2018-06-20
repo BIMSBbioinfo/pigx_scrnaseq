9d0f75ceccc86965107875c29e33138d7681f2d7
Franke <vedran.franke@gmail.com>
2018-06-20 18:58:04 +0200

    scripts/Extract_Read_Statistics.R: removed dropbead dependency
    scripts/Extract_Read_Statistics.R:
    	- .extractReadStatistics - calculates all of the statistics inhouse

--- scripts/Extract_Read_Statistics.R
+++ scripts/Extract_Read_Statistics.R
@@ -3,6 +3,42 @@ options = commandArgs(trailingOnly=TRUE)
 source(file.path(options[2],'/Argument_Parser.R'))
 argv = Parse_Arguments('Extract_Read_Statistics')
 
+
+# -------------------------------------------------------------------------- #
+.extractReadStatistics = function(
+  bamfile,
+  yieldSize = 1e6,
+  norm.factor = 1e6,
+  read_categories = c('INTRONIC','INTERGENIC','CODING','UTR')
+  ){
+
+    bam = scanBam(BamFile(bamfile), yieldSize=yieldSize,
+                          param=ScanBamParam(
+                            what='mapq',
+                            tag=c('XF', 'GE', 'XC')
+                          ))[[1]]
+
+    default = setNames(rep(0, length(read_categories)),read_categories)
+    non.ge.types = table(bam$tag$XF[which(bam$mapq == 255 & is.na(bam$tag$GE))])
+    if(length(non.ge.types == 0)){
+        non.ge.types = default
+
+    }else{
+        default[names(non.ge.types)] = non.ge.types
+        non.ge.types = default
+    }
+
+
+    df = data.frame('total_reads'    = round(length(bam$mapq)/norm.factor, 4),
+                    'uniq_mapped'    = round(sum(bam$mapq == 255, na.rm = T)/norm.factor, 1),
+                    'intronic'       = round(non.ge.types['INTRONIC']/norm.factor, 4),
+                    'intergenic'     = round(non.ge.types['INTERGENIC']/norm.factor, 4),
+                    'coding'         = round(non.ge.types['CODING']/norm.factor, 4),
+                    'UTR'            = round(non.ge.types['UTR']/norm.factor, 4),
+                    'total_barcodes' = round(length(unique(bam$tag$XC[which(bam$mapq == 255)]))/norm.factor, 4))
+    return (df)
+  }
+
 # -------------------------------------------------------------------------- #
 Extract_Read_Statistics = function(
     bamfile = NULL,
@@ -16,11 +52,10 @@ Extract_Read_Statistics = function(
   if(is.null(outfile))
       stop('outfile not specified')
 
-  suppressPackageStartupMessages(library(dropbead))
   suppressPackageStartupMessages(library(Rsamtools))
 
     message('Extracting read statistics ...')
-      stat = extractReadStatistics(bamfile)
+      stat = suppressWarnings(.extractReadStatistics(bamfile))
       rownames(stat) = outname
 
     message('Writing read statistics ...')
